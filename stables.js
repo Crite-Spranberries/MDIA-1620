b6d64a7a9954d1b489ad499709b584d710e7cef5
Crite-Spranberries <113459843+Crite-Spranberries@users.noreply.github.com>
2024-09-19 15:41:36 -0700

    Added stables.js content

---
+++ stables.js
@@ -0,0 +1,14 @@
+console.log("Horses are great!");
+
+// Initializations
+let horseName = "Coolguy";
+horseName = "Tennessee Walker";
+let horseAge = 5;
+
+// Stable status
+let isHorseInStable = true;
+isHorseInStable = false;
+
+// Print
+console.log(horseName, horseAge, isHorseInStable);
+
