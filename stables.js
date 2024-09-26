7f470b32ea51654e7e9fe2adbd919fb18e5a512b
Crite-Spranberries <113459843+Crite-Spranberries@users.noreply.github.com>
2024-09-26 14:32:04 -0700

    Added some more content to stables.js

--- stables.js
+++ stables.js
@@ -1,14 +1,32 @@
-console.log("Horses are great!");
+/*<- - - - - - - - - - - - Lesson 3 - - - - - - - - - - - - - >*/
 
-// Initializations
-let horseName = "Coolguy";
-horseName = "Tennessee Walker";
-let horseAge = 5;
+// console.log("Horses are great!");
 
-// Stable status
-let isHorseInStable = true;
-isHorseInStable = false;
+// // Initializations
+// let horseName = "Coolguy";
+// horseName = "Tennessee Walker";
+// let horseAge = 5;
 
-// Print
-console.log(horseName, horseAge, isHorseInStable);
+// // Stable status
+// let isHorseInStable = true;
+// isHorseInStable = false;
 
+// // Print
+// console.log(horseName, horseAge, isHorseInStable);
+
+/*<- - - - - - - - - - - - Lesson 4 - - - - - - - - - - - - - >*/
+
+// console.log(typeof 999);
+// console.log(typeof "890");
+// console.log(typeof true);
+// console.log(typeof horseAge);
+
+const STABLE_MONTHLY_FEE = 500;
+console.log(STABLE_MONTHLY_FEE);
+console.log(STABLE_MONTHLY_FEE - 50);
+console.log(STABLE_MONTHLY_FEE *.10);
+
+let owner = "Arthur Morgan";
+let horse = "Tenn Walker";
+let stableIntro = `This stable costs $${STABLE_MONTHLY_FEE} for ${owner}'s "${horse}".`
+console.log(stableIntro);
